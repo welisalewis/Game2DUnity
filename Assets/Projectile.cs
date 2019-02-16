6effe487c11797f09a3e88dbf80ed66548954d2c
welisalewis <welisalewis1@gmail.com>
2019-02-16 23:00:19 +0530

    Bullets firing

---
+++ Assets/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float speed; //speed of moving forward
+	public float lifeTime; //Lifetime of the projectile
+
+    public GameObject destroyEffect;
+
+	private void start(){
+
+		Invoke("DestroyProjectile",lifeTime);
+	}
+
+	private void Update(){
+
+		transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile(){
+    	Instantiate(destroyEffect,transform.position,Quaternion.identity);
+    	Destroy(gameObject);
+    }
+
+}
