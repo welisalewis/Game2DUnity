6423768449e8edccadf9a54d8881cbe812a68ba1
welisalewis <welisalewis1@gmail.com>
2019-02-17 11:28:34 +0530

    fixed bullet shooting

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -16,7 +16,7 @@ public class Projectile : MonoBehaviour {
 
 	private void Update(){
 
-		transform.Translate(Vector2.up * speed * Time.deltaTime);
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
     void DestroyProjectile(){
